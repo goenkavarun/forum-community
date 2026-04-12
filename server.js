6311e9b19b48ded0f085976f75c80ca13efa0ac6
goenkavarun <121847058+goenkavarun@users.noreply.github.com>
2026-04-12 18:58:09 +0530

    Update server.js update in security

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 // Security middleware
 app.use(helmet());
+app.set('trust proxy', 1);
 app.use(cors({
     origin: [
         'indiadigitalmarketingforum.org',
